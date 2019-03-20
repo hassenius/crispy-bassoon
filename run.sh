576a54fc142c906b646343d40a3d225015e77433
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-03-20 15:56:16 +0100

    change org mngr user

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-[[ -z ${team_admin_user} ]] && read -p "Team admin user: " team_admin_user
-[[ -z ${team_admin_pass} ]] && read -s -t 60 -p "Team admin passwor: " team_admin_pass
+[[ -z ${org_mngr_user} ]] && read -p "Org manager user: " org_mngr_user
+[[ -z ${org_mngr_pass} ]] && read -s -t 60 -p "Org manager password: " org_mngr_pass
 echo "\n "
 [[ -z ${team_op_user} ]] && read -p "Team operator user: " team_op_user
 [[ -z ${team_op_pass} ]] && read -s -t 60 -p "Team operator password: " team_op_pass
 echo "\n "
 
 
-if [[ -z ${team_admin_user} || -z ${team_admin_pass} || -z ${team_op_user} || -z ${team_op_pass} ]]; then
+if [[ -z ${org_mngr_user} || -z ${org_mngr_pass} || -z ${team_op_user} || -z ${team_op_pass} ]]; then
   echo "Missing username password input"
   exit 0
 fi
-echo "${team_admin_user} ${team_admin_pass} ${team_op_user} ${team_op_pass}"
+echo "${org_mngr_user} ${org_mngr_pass} ${team_op_user} ${team_op_pass}"
