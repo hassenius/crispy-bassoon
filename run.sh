1a9c55365d2a987067502099122a0b6a60059791
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2019-03-20 17:48:34 +0100

    fix syntax

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the working directory
-APP_ROOT="$(dirname "$($(type -p greadlink readlink | head -1) -f  "$BASH_SOURCE")")"
+export APP_ROOT="$(dirname "$($(type -p greadlink readlink | head -1) -f  "$BASH_SOURCE")")"
 
 # Get defaults
 source ${APP_ROOT}/defaults.sh
@@ -10,10 +10,10 @@ source ${APP_ROOT}/defaults.sh
 [[ -z ${org_name} ]] && read -t 60 -p "Organisation name: " org_name
 [[ -z ${org_mngr_user} ]] && read -t 60 -p "Org manager user: " org_mngr_user
 [[ -z ${org_mngr_pass} ]] && read -s -t 60 -p "Org manager password: " org_mngr_pass
-echo "\n "
+echo -e "\n"
 [[ -z ${team_op_user} ]] && read -t 60 -p "Team operator user: " team_op_user
 [[ -z ${team_op_pass} ]] && read -s -t 60 -p "Team operator password: " team_op_pass
-echo "\n "
+echo -e "\n "
 [[ -z ${portfolio_name} ]] && read -t 60 -p "Portfolio_name: " portfolio_name
 
 if [[ -z ${org_name} || -z ${org_mngr_user} || -z ${org_mngr_pass} || -z ${team_op_user} || -z ${team_op_pass} || -z ${portfolio_name} ]]; then
